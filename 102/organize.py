3567c91fdb2be26c328f0947011bd7d5b3f49fde
RicardoStumm-Dev <130321637+RicardoStumm-Dev@users.noreply.github.com>
2023-04-09 20:33:23 -0300

    Add files via upload

---
+++ 102/organize.py
@@ -0,0 +1,29 @@
+import os
+import shutil
+
+from_dir = "C:/Users/Ricardo/Downloads"
+to_dir = "C:/Users/Ricardo/Downloads/Imagens"
+
+list_of_files = os.listdir(from_dir)
+# print(list_of_files)
+
+for file_name in list_of_files:
+
+    name, extension = os.path.splitext(file_name)
+
+    if extension == '':
+      continue
+    if extension in ['.png','.jpeg','.gif','.jpg','.jfif']:
+      path1 = from_dir + '/' + file_name
+      path2 = to_dir + '/' + "Arquivos Imagem"
+      path3 = to_dir + '/' + "Arquivos Imagem" + '/' + file_name
+      if os.path.exists(path2):
+        print("Movendo"+ file_name + ". . . . .")
+
+        shutil.move(path1,path3)
+      else:
+        os.makedirs(path2)
+    
+        print("Movendo"+ file_name + ". . . . .")
+
+        shutil.move(path1,path3)
